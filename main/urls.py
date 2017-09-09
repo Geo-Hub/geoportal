72947c5a8d1b4d9bd2e851b4a9650c803454ece0
Victor Ngeno <ngenovictor321@gmail.com>
2017-09-09 19:47:54 +0300

    update urls.py with access to static files

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     url(r'^$', views.home,name='home'),
-    
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# will enable below settings once I split local and production settings
+
+# if not settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
