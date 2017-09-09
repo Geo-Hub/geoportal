e207390acb36a10f8fc30b266a42ee7a23212361
Victor Ngeno <ngenovictor321@gmail.com>
2017-09-09 19:33:07 +0300

    updated settings with GDAL settings and DB Engine for heroku

--- geoportal/settings.py
+++ geoportal/settings.py
@@ -155,8 +155,7 @@ except ImportError:
 # add this
 db_from_env = dj_database_url.config()
 if db_from_env:
-    DATABASES = {
-        'default': {
-            db_from_env
-        }
-    }
+    GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+    GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+    DATABASES['default'] = dj_database_url.config()
+    DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
