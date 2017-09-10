0aa89a5ab6bfe05f343de1a0dde6d8edc2ef91f7
Victor Ngeno <ngenovictor321@gmail.com>
2017-09-10 17:38:47 +0300

    update dropdown links on base templates

--- main/forms.py
+++ main/forms.py
@@ -1,20 +1,22 @@
 from django.contrib.gis import forms
 from .models import *
 
+
 class ContactMessageForm(forms.ModelForm):
-	class Meta:
-		model = ContactMessage
-		
-		fields = [
-		'name_of_sender',
-		'email_address',
-		'message',
-		]
+    class Meta:
+        model = ContactMessage
+
+        fields = [
+            'name_of_sender',
+            'email_address',
+            'message',
+        ]
+
 
 class RegistrationForm(forms.ModelForm):
-	class Meta:
-		model = Registration
-		fields = [
-		'submitted_from',
-		'parcel_no',
-		]
+    class Meta:
+        model = Registration
+        fields = [
+            'submitted_from',
+            'parcel_no',
+        ]
