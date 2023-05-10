f1665c02416454c0d23f2cc4818666bb496fd3ab
victor ngeno <ngenovictor321@gmail.com>
2023-05-10 22:01:52 +0300

    manage using an entry point

---
+++ build/web/entry_point.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "${0}: running migrations."
+python manage.py makemigrations --merge
+python manage.py migrate --noinput
+
+# echo "${0}: collecting statics."
+
+# python manage.py collectstatic --noinput
+
+gunicorn --bind 0.0.0.0:8000 geoportal.wsgi
+
+# gunicorn yourapp.wsgi:application \
+#     --env DJANGO_SETTINGS_MODULE=yourapp.production_settings \
+#     --name yourapp \
+#     --bind 0.0.0.0:8000 \
+#     --timeout 600 \
+#     --workers 4 \
+#     --log-level=info \
+#     --reload
