69ce891152950eddf6886f99d22d86856c84fc46
victor ngeno <ngenovictor321@gmail.com>
2023-05-14 13:49:44 +0300

    add debug messages

--- build/web/entry_point.sh
+++ build/web/entry_point.sh
@@ -14,16 +14,20 @@ do
     sleep 1
 done
 
+echo "Postgres server is running proceeding."
+echo "-------------------------------------------------------------------"
 echo "${0}: running migrations."
-python manage.py makemigrations --merge
+python manage.py makemigrations --merge --noinput --check
 python manage.py migrate --noinput
 
-#echo "${0}: collecting statics."
-#
-#python manage.py collectstatic --noinput
+echo "${0}: collecting statics."
 
+python manage.py collectstatic --noinput
+
+echo "Loading GIS data into the DB."
 python manage.py load_gis_data
 
+echo "Starting webserver"
 gunicorn --bind 0.0.0.0:8000 geoportal.wsgi
 
 # gunicorn yourapp.wsgi:application \
