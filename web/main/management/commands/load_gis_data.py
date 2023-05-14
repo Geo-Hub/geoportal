b9d41021d1ab7804493693714e3504e3d76c015b
victor ngeno <ngenovictor321@gmail.com>
2023-05-14 13:57:07 +0300

    reformat the scripts and add them to run

--- web/main/management/commands/load_gis_data.py
+++ web/main/management/commands/load_gis_data.py
@@ -1,9 +1,16 @@
 from typing import Any, Optional
 from django.core.management.base import BaseCommand, CommandError, CommandParser
 
+
 class Command(BaseCommand):
     help = "Loads the shapefile data into the relevant DB tables."
 
-    def handle(self, *args: Any, **options: Any) -> str | None:
-        from main.management.load_scripts import load_county
+    def handle(self, *args: Any, **options: Any):
+        from main.management.load_scripts import (
+            load_county,
+            load_constituencies,
+            load_shambas
+        )
         load_county.run()
+        load_constituencies.run()
+        load_shambas.run()
--- web/main/management/load_scripts/load_shambas.py
+++ web/main/management/load_scripts/load_shambas.py
@@ -1,36 +1,33 @@
-import os
-
-from django.core.management.base import BaseCommand, CommandError
-from polls.models import Question as Poll
+from pathlib import Path
 
 from django.contrib.gis.utils import LayerMapping
-from models import Shamba
+from main.models import Shamba
 
 # Auto-generated `LayerMapping` dictionary for Shamba model
-
 shamba_mapping = {
-    'objectid_1' : 'OBJECTID_1',
-    'objectid' : 'OBJECTID',
-    'shape_leng' : 'SHAPE_Leng',
-    'y_coordina' : 'Y_coordina',
-    'x_coordina' : 'X_coordina',
-    'perimeter' : 'Perimeter',
-    'parcel_no' : 'parcel_No',
-    'owner' : 'Owner',
-    'soil_type' : 'soil_type',
-    'state' : 'State',
-    'cost_value' : 'cost_value',
-    'pic_url' : 'pic_url',
-    'electricit' : 'Electricit',
-    'water' : 'Water',
-    'outlinetra' : 'OutlineTra',
-    'shape_le_1' : 'Shape_Le_1',
-    'shape_area' : 'Shape_Area',
-    'geom' : 'MULTIPOLYGON',
+    'objectid_1': 'OBJECTID_1',
+    'objectid': 'OBJECTID',
+    'shape_leng': 'SHAPE_Leng',
+    'y_coordina': 'Y_coordina',
+    'x_coordina': 'X_coordina',
+    'perimeter': 'Perimeter',
+    'parcel_no': 'parcel_No',
+    'owner': 'Owner',
+    'soil_type': 'soil_type',
+    'state': 'State',
+    'cost_value': 'cost_value',
+    'pic_url': 'pic_url',
+    'electricit': 'Electricit',
+    'water': 'Water',
+    'outlinetra': 'OutlineTra',
+    'shape_le_1': 'Shape_Le_1',
+    'shape_area': 'Shape_Area',
+    'geom': 'MULTIPOLYGON',
 }
 
-shamba_shp = os.path.abspath(os.path.join(os.path.dirname(__file__),
-	'data/realwgs84.shp'))
-def run (verbose=True):
-	lm = LayerMapping(Shamba, shamba_shp, shamba_mapping,transform=False,encoding='iso-8859-1')
-	lm.save(strict=True, verbose=verbose)
+shamba_shp = (Path(__file__).parent / 'data/realwgs84.shp').as_posix()
+
+
+def run(verbose=True):
+    lm = LayerMapping(Shamba, shamba_shp, shamba_mapping, transform=False, encoding='iso-8859-1')
+    lm.save(strict=True, verbose=verbose)
