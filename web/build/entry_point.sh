6b1bafcbba5801e7bb16655c5c92983cd7bb074e
victor ngeno <ngenovictor321@gmail.com>
2025-03-26 16:23:21 +0300

    move build files around. Update the gdal image used

--- web/build/entry_point.sh
+++ web/build/entry_point.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 set -e
+
 # ensure the postgress server is running.
 echo "Checking status for postgres server."
 db_connection_test=1
 while [ $db_connection_test -eq 1 ]
 do
     set +e
-    python db_connection_test.py
+    venv/bin/python db_connection_test.py
     db_connection_test=$?
     set -e
     echo test result: $db_connection_test
@@ -17,15 +18,19 @@ done
 echo "Postgres server is running proceeding."
 echo "-------------------------------------------------------------------"
 echo "${0}: running migrations."
-python manage.py makemigrations --merge --noinput --check
-python manage.py migrate --noinput
+venv/bin/python manage.py makemigrations --merge --noinput --check
+venv/bin/python manage.py migrate --noinput
 
 echo "${0}: collecting statics."
 
-python manage.py collectstatic --noinput
+venv/bin/python manage.py collectstatic --noinput
 
 echo "Loading GIS data into the DB."
-python manage.py load_gis_data
+unzip data.zip -d /app/main/management/load_scripts/
+venv/bin/python manage.py load_gis_data
+
+rm -rf /app/main/management/load_scripts/data
+rm data.zip
 
 echo "Starting webserver"
-gunicorn --bind 0.0.0.0:8000 geoportal.wsgi
+venv/bin/python -m gunicorn --bind 0.0.0.0:8000 geoportal.wsgi
