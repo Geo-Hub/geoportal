09a8a68b8398b2cae5d58c39cb16c57d1a8b5eb1
victor ngeno <ngenovictor321@gmail.com>
2025-03-26 23:29:17 +0300

    update settings for static

--- web/geoportal/settings.py
+++ web/geoportal/settings.py
@@ -97,11 +97,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static","static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static","our_static"),
+    os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
     )
 
