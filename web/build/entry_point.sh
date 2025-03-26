6f9ef67c627799793e17999600cba390a4b9d1db
victor ngeno <ngenovictor321@gmail.com>
2025-03-26 19:50:53 +0300

    only ten retries

--- web/build/entry_point.sh
+++ web/build/entry_point.sh
@@ -5,15 +5,23 @@ set -e
 # ensure the postgress server is running.
 echo "Checking status for postgres server."
 db_connection_test=1
-while [ $db_connection_test -eq 1 ]
+num_retries=0
+while [ $db_connection_test -eq 1 ] && [ $num_retries -lt 10 ]
 do
     set +e
     venv/bin/python db_connection_test.py
     db_connection_test=$?
     set -e
-    echo test result: $db_connection_test
+    num_retries=$((num_retries + 1))
+    echo test result: $num_retries failed
     sleep 1
 done
+
+if [ $db_connection_test -eq 1 ]
+then
+    echo "Postgres server is not running after $num_retries retries."
+    exit 1
+fi
 
 echo "Postgres server is running proceeding."
 echo "-------------------------------------------------------------------"
