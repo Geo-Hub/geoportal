7b2d29f58de3c54a39abd451f7e974eb077db489
victor ngeno <ngenovictor321@gmail.com>
2025-03-26 23:30:12 +0300

    style. Don't show names if not admin

--- web/main/views.py
+++ web/main/views.py
@@ -1,30 +1,32 @@
 from django.core.mail import EmailMessage
-from django.shortcuts import render
-from .models import *
-from .forms import LandRegistrationForm
-from django.http import HttpResponseRedirect
 from django.core.serializers import serialize
+from django.db.models import Value
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import LandRegistrationForm
+from .models import *
 
 
 # Create your views here.
 def home(request):
-    title = 'Home'
-    template = 'home.html'
-    context = {'title': title}
+    title = "Home"
+    template = "home.html"
+    context = {"title": title}
     return render(request, template, context)
 
 
 def about(request):
-    title = 'About'
-    template = 'about.html'
-    context = {'title': title}
+    title = "About"
+    template = "about.html"
+    context = {"title": title}
     return render(request, template, context)
 
 
 def contact(request):
-    title = 'Contact'
+    title = "Contact"
     helpmessage = None
-    if request.method == 'POST':
+    if request.method == "POST":
         intention = request.POST.get("intention")
         if intention == "submit_form":
             showform = False
@@ -32,80 +34,83 @@ def contact(request):
             email_address = request.POST.get("email_address")
             message = request.POST.get("message")
             if name and message and email_address:
-                message = "Hey good people,\nThis is a message from the contact form at" \
-                    "geohub-geoportal.herokuapp.com, beware and don't click any links in the" \
-                    "message or be cautious.\n-----------------------------\n" \
+                message = (
+                    "Hey good people,\nThis is a message from the contact form at"
+                    "geohub-geoportal.herokuapp.com, beware and don't click any links in the"
+                    "message or be cautious.\n-----------------------------\n"
                     "contact email address: {}\n\n{}".format(email_address, message)
+                )
                 email_message = EmailMessage(
                     subject="Feedback on Geohub Geoportal by " + name,
                     body=message,
-                    to=["ngenovictor321@gmail.com", "jimmywainaina@gmail.com", "thomas.muteti@gmail.com",
-                        "njerimurage92@gmail.com", "nombumurage@gmail.com"],
+                    to=[
+                        "ngenovictor321@gmail.com",
+                        "jimmywainaina@gmail.com",
+                        "thomas.muteti@gmail.com",
+                        "njerimurage92@gmail.com",
+                        "nombumurage@gmail.com",
+                    ],
                 )
                 email_message.send()
 
-                helpmessage = 'Thank you for your feedback.'
-                context = {
-                    'title': title,
-                    'helpmessage': helpmessage,
-                    'showform': showform
-                }
-                return render(request, 'contact.html', context)
+                helpmessage = "Thank you for your feedback."
+                context = {"title": title, "helpmessage": helpmessage, "showform": showform}
+                return render(request, "contact.html", context)
         else:
-            helpmessage = 'Fill the form below'
+            helpmessage = "Fill the form below"
             showform = True
-            context = {
-                'title': title,
-                'helpmessage': helpmessage,
-                'showform': showform
-            }
+            context = {"title": title, "helpmessage": helpmessage, "showform": showform}
 
-            return render(request, 'contact.html', context)
+            return render(request, "contact.html", context)
 
-    return render(request, 'contact.html', {})
+    return render(request, "contact.html", {})
 
 
 def data(request):
-    template = 'lookupdata.html'
-    title = 'Shamba'
-    shambas = serialize('geojson', Shamba.objects.all())
-    counties = serialize('geojson', KiambuCounty.objects.all())
-    constituencies = serialize('geojson', KiambuConstituencies.objects.all())
+    template = "lookupdata.html"
+    title = "Shamba"
+    shambas = Shamba.objects.all()
+    if not request.user.is_superuser:
+        shambas = serialize(
+            "geojson", shambas, fields=[f.name for f in Shamba._meta.fields if f.name != "owner"]
+        )
+    else:
+        shambas = serialize("geojson", shambas)
+    counties = serialize("geojson", KiambuCounty.objects.all())
+    constituencies = serialize("geojson", KiambuConstituencies.objects.all())
 
     context = {
-        'title': title,
-        'shambas': shambas,
-        'counties': counties,
-        'constituencies': constituencies
+        "title": title,
+        "shambas": shambas,
+        "counties": counties,
+        "constituencies": constituencies,
     }
     return render(request, template, context)
 
 
 def registershamba(request):
-    template = 'registershamba.html'
-    title = 'Register'
+    template = "registershamba.html"
+    title = "Register"
     reg_form = LandRegistrationForm()
-    context = {
-        'title':title,
-        'reg_form': reg_form
-    }
-    if request.method == 'POST':
+    context = {"title": title, "reg_form": reg_form}
+    if request.method == "POST":
         pass
     else:
         return render(request, template, context)
 
+
 def update_payment(request):
-    template = 'update-payment.html'
-    shambas = Shamba.objects.all().order_by('-parcel_no')[:20]
+    template = "update-payment.html"
+    shambas = Shamba.objects.all().order_by("-parcel_no")[:20]
     message = None
     if request.method == "POST":
-        post_type = request.POST.get('post-type')
+        post_type = request.POST.get("post-type")
         if post_type == "yearly-update":
             if request.user.is_staff:
                 for shamba in Shamba.objects.all():
                     try:
                         value = int(shamba.balance)
-                        new_bal = value+100
+                        new_bal = value + 100
                         shamba.balance = new_bal
                         shamba.save()
                     except Exception:
@@ -116,13 +121,13 @@ def update_payment(request):
                 message = "You need to be staff to take that action"
         elif post_type == "payment-update":
             if request.user.is_staff:
-                arrears = request.POST.get('arrears-update')
-                payment = request.POST.get('payment-update')
-                shamba_id = request.POST.get('shamba-id')
+                arrears = request.POST.get("arrears-update")
+                payment = request.POST.get("payment-update")
+                shamba_id = request.POST.get("shamba-id")
                 shamba_obj = Shamba.objects.get(id=int(shamba_id))
                 try:
-                    new_bal = int(shamba_obj.balance) # db has saved int value
-                except Exception: # db has maybe none in db
+                    new_bal = int(shamba_obj.balance)  # db has saved int value
+                except Exception:  # db has maybe none in db
                     new_bal = 0
                 if payment:
                     new_bal -= int(payment)
@@ -133,40 +138,35 @@ def update_payment(request):
                 message = "Details for user {} has been updated.".format(shamba_obj.shamba_owner)
             else:
                 message = "You need to be staff to take that action"
-    context = {
-        'shambas':shambas,
-        'message':message
-    }
+    context = {"shambas": shambas, "message": message}
     return render(request, template, context)
 
+
 def discardshamba(request):
-    template = 'discardshamba.html'
-    title = 'Discard'
+    template = "discardshamba.html"
+    title = "Discard"
     # shambas = OwnershipInfo.objects.all()
     context = {
-        'title': title,
+        "title": title,
         # 'shambas':shambas,
     }
     return render(request, template, context)
 
 
 def changeowner(request):
-    template = 'changeowner.html'
-    title = 'Change'
+    template = "changeowner.html"
+    title = "Change"
     context = {
-        'title': title,
+        "title": title,
     }
     return render(request, template, context)
 
 
 def statistics(request):
-    template = 'statistics.html'
-    title = 'Statistics'
+    template = "statistics.html"
+    title = "Statistics"
     # Step 1: Create a DataPool with the data we want to retrieve.
-    order_by = request.GET.get('order_by', 'id')
-    data = Shamba.objects.all().order_by('id')
-    context = {
-        'title': title,
-        'data': data
-    }
+    order_by = request.GET.get("order_by", "id")
+    data = Shamba.objects.all().order_by("id")
+    context = {"title": title, "data": data}
     return render(request, template, context)
