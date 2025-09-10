8fb843333cf3adf6d1993bfcb33af6d38b43a2f0
victor ngeno <ngenovictor321@gmail.com>
2025-09-10 16:47:52 +0300

    update

--- web/build/db_connection_test.py
+++ web/build/db_connection_test.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     try:
         conn = psycopg.connect()
         print("Successful connection.")
-    except psycopg.OperationalError:
-        print("Connection failed.")
+    except Exception as e:
+        print(f"Connection failed: {e}")
         exit(1)
